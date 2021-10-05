78f6f7056785cbfd811fe416f56e9aa3fde60201
AbdulSamad Ayoade <ayoadesamad@gmail.com>
2021-10-05 17:00:40 +0100

    Added search functionality

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=d990fa1699
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
 
   useEffect(() => {
     fetch(FEATURED_API)
@@ -15,13 +16,31 @@ function App() {
     });
   }, []);
 
-  console.log(movies)
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if(searchTerm) {
+      fetch(SEARCH_API + searchTerm)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovies(data.results);
+      });
+
+      setSearchTerm('');
+    }
+  }
+
+  const handleOnChange = (e) => {
+    setSearchTerm(e.target.value)
+  }
 
   return (
     <>
       <header>
         <a href="/">Movies App</a>
-        <input type="text" className="search" placeholder="Search" />
+        <form onSubmit={handleOnSubmit}>
+          <input type="text" className="search" placeholder="Search" value={searchTerm} onChange={handleOnChange}/>
+        </form>
       </header>
       <div className="movie-container">
         {movies.length > 0 && movies.map((movie) => <Movie key={movie.id} {...movie} />)}
